d61407cdf5546644c14ec268099074731d6dfdf4
Eswar Rao <eswarraokdipelly@gmail.com>
2021-04-19 23:27:28 +0530

    Final Output

---
+++ src/components/emojis/index.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './style.css'
+
+function Emoji(props) {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  const handleClick = e => {
+    setFadeIn(true)
+    setTimeout(() => setFadeIn(false), 900)
+  }
+
+  return (
+    <div
+      key={props.id}
+      className="item"
+      // title="Copy Emoji to Clipboard"
+      onClick={handleClick}
+      data-clipboard-text={props.symbol}
+      data-keywords={props.keywords}
+    >
+      <div className={`item-copy ${fadeIn?'label-true':'label-false'}`}>
+        Copied!
+      </div>
+      <span className="item-emoji">{props.symbol}</span>
+    </div>
+  )
+}
+
+export default Emoji;
